e0ff91d93560efbd64cd9e394066776cfa2cb89d
Jericho Lawson <jboloslawson@gmail.com>
2021-08-16 18:13:08 -0700

    Updates to Stage 3 Code
    - Added new algorithms to functions code (e.g. LightGBM, SVM, RandomForest)
    - Added new comments
    - Still need to add more comments and refine code; but the code runs as intended currently

--- Code/STAGE3functions.R
+++ Code/STAGE3functions.R
@@ -2,70 +2,160 @@
 ## Summer 2019, 2021
 ## AFib Functions for Training Program
 
+# Functions that aid the STAGE3training.R code to process the data, train
+# various models, and output success metrics.
 
+#### Utility Functions #########################################################
 
-#### LIBRARIES #################################################################
+# Function to open a specific library depending on which algorithms are used.
+# I: pick (3-letter code indicating the algorithm in use)
+# O: none
+library_selector = function(pick){
+  if (pick == "LDA" | pick == "QDA"){ # LDA/QDA
+    library(MASS)
+  } else if (pick == "GBM"){ # gradient boosting
+    library(gbm)
+  } else if (pick == "XGB"){ # XGBoost
+    library(xgboost)
+  } else if (pick == "LGB"){ # light graident boosting (LightGBM)
+    library(lightgbm)
+  } else if (pick == "SVM"){ # support-vector machine
+    library(e1071)
+  } else if (pick == "RFO"){ # random forest
+    library(randomForest)
+  }
+}
 
-packages = c("MASS", "gbm", "scales", "ggplot2", "reshape2", "ggpubr", "tidyr")
-lapply(packages, library, character.only = TRUE)
-
+# Creates formula based on user-specified model.
+# I: indices (indices of covariates to be used from data frame)
+#    cols (names of columns from data frame)
+# O: formula type that has the model that will be used
 formula_maker = function(indices, cols){
   names = cols[indices]
   return(formula(paste("State ~", paste(names, collapse = "+"))))
 }
 
+# Labels subjects (randomly for 2017/CinC dataset, by subject for MIT-BIH)
+# for k-fold cross validation.
+# I: data (data frame in use), vers ("M" or "C"),
+#    seed (user-generated seed for randomization), 
+#    k (number of folds, 2017 data only)
+#    cols (names of columns from data frame)
+# O: Numeric vector that labels which observation is in which fold for
+#    cross-validation purposes
 grouper = function(data, vers, seed, k){
-  if (vers == "M"){
+  if (vers == "M"){ # MIT-BIH data
     return(as.numeric(factor(data$Subject)))
-  } else {
+  } else { # 2017/CinC data
     set.seed(seed)
     return(sample(rep(1:k, length.out = dim(data)[1])))
   }
 }
 
-#"LOG", "LDA", "QDA", "GBM"
-algorithm_picker = function(pick, formula, train, test, thresh, seed){
-  if (pick == "LOG"){
+# Chooses which function to run based on the user-specified algorithm.
+# I: pick (3-letter code indicating the algorithm in use),
+#    formula (model used for algorithm),
+#    index_c (indices in data frame that correspond to covariate data),
+#    index_r (index in data frame that corresponds to response data),
+#    train (training data),
+#    test (testing data),
+#    thresh (probabilistic cutoff point that classifies segment as AFib or N),
+#    seed (seed for random generation)
+# O: Results from algorithm (list containing true/predicted states and time)
+algorithm_picker = function(pick, formula, index_c, index_r, train, 
+                            test, thresh, seed){
+  if (pick == "LOG"){ # logistic regression
     logistic(formula, train, test, thresh, seed)
-  } else if (pick == "LDA"){
+  } else if (pick == "LDA"){ # LDA
     lda_f(formula, train, test, seed)
-  } else if (pick == "QDA"){
+  } else if (pick == "QDA"){ # QDA
     qda_f(formula, train, test, seed)
-  } else if (pick == "GBM"){
+  } else if (pick == "GBM"){ # gradient boosting
     boost_gbm(formula, train, test, thresh, seed)
-  } 
+  } else if (pick == "XGB"){ # XGBoost
+    boost_xgb(index_c, index_r, train, test, 
+              nrounds = 5000, eta = 0.01, gamma = 0, 
+              max_depth = 4, min_child_weight = 1, subsample = 1, 
+              colsample_bytree = 1, lambda = 0, alpha = 1, 
+              thresh = 0.5, seed = seed)
+  } else if (pick == "LGB"){ # light gradient boosting (LightGBM)
+    boost_lgb(index_c, index_r, train, test, nrounds = 100, type = "gbdt", 
+              num_leaves = 31, max_depth = -1, 
+              num_threads = 0, thresh, seed)
+  } else if (pick == "SVM"){ # support-vector machine
+    svm_f(formula, index_c, train, test, kernel = "linear", cost = 1, seed)
+  } else if (pick == "RFO"){ # random forest
+    rf_f(formula, train, test, ntrees = 500, seed)
+  }
 }
 
-# Function that runs all testing.
-testing = function(pick, data, rand, formula, thresh, seed, folds = max(rand)){
+# Runs all testing for k-fold cross validation and times the process. 
+# Returns true/predicted states of each fold, as well as how long it took 
+# to run all folds combined (in seconds).
+# I: pick (3-letter code indicating the algorithm in use),
+#    data (data set used)
+#    rand (vector of labels specifying fold for cross-validation purposes)
+#    formula (model used for algorithm),
+#    index_c (indices in data frame that correspond to covariate data),
+#    index_r (index in data frame that corresponds to response data),
+#    thresh (probabilistic cutoff point that classifies segment as AFib or N),
+#    seed (seed for random generation),
+#    folds (number of folds needed for k-fold cross validation)
+# O: List containing true/predicted states of observation in each fold in
+#    the first element and the time it took to run k-fold cross validation
+#    in the second element.
+testing = function(pick, data, rand, formula, index_c, index_r, thresh, 
+                   seed, folds = max(rand)){
+  # create empty list for results and records beginning time
   results = list()
+  time = Sys.time()
   
-  for (i in 1:folds){
-    samp = which(rand == i); train = data[-samp, ]; test = data[samp, ]
-    results[[i]] = algorithm_picker(pick, formula = formula, train = train, test = test, thresh = 0.5, seed = seed)
+  for (i in 1:folds){ # for each fold
+    # creates training/testing sets
+    samp = which(rand == i); train = data[-samp, ]; test = data[samp, ] 
+    
+    # runs algorithm, assigns results to ith element of list
+    results[[i]] = algorithm_picker(pick, formula = formula, index_c = index_c, 
+                                    index_r = index_r, train = train, 
+                                    test = test, thresh = 0.5, seed = seed)
   }
   
-  return(results)
+  # returns list of results and the length of time involved in running this 
+  # process
+  return(list("results" = results, 
+              "time" = as.numeric(difftime(Sys.time(), time, units = "secs"))))
 }
 
-metrics = function(list){
-  for (fold in 1:length(list)){
+# Using the list of results and times, the function outputs various metrics
+# for verifying the success of a model, such as sensitivity, prediction
+# accuracy, and F-1 score.
+# I: list (contains true/predicted states of observation in each fold in
+#          the first element and the time it took to run k-fold cross 
+#          validation in the second element)
+# O: List containing true/predicted states of observation in each fold in
+#    the first element and the time it took to run k-fold cross validation
+#    in the second element.
+metrics_f = function(list){
+  # create confusion matrix
+  for (fold in 1:length(list$results)){
     if (fold == 1){
-      confusion = table(list[[fold]]$Truth, list[[fold]]$Pred)
+      confusion = table(list$results[[fold]]$Truth, list$results[[fold]]$Pred)
     } else {
-      confusion = confusion + table(list[[fold]]$Truth, list[[fold]]$Pred)
+      confusion = confusion + table(list$results[[fold]]$Truth, list$results[[fold]]$Pred)
     }
   }
   
-  # works when there are tallies of zero!
+  # Returns list of metrics.
+  # Note: works when there are tallies of zero!
   return(list("confusion" = confusion, 
               "sensitivity" = confusion[2, 2] / sum(confusion[2, ]),
               "specificity" = confusion[1, 1] / sum(confusion[1, ]),
               "f1" = confusion[2, 2] / (confusion[2, 2] + 0.5 * (confusion[1, 2] + confusion[2, 1])),
-              "accuracy" = (confusion[1, 1] + confusion[2, 2]) / sum(confusion)))
+              "accuracy" = (confusion[1, 1] + confusion[2, 2]) / sum(confusion),
+              "time" = list$time))
 }
 
-#### ALGORITHMS ################################################################
+#### Algorithms ################################################################
 
 # Logistic regression testing.
 logistic = function(formula, train, test, thresh, seed){
@@ -127,31 +217,82 @@ boost_gbm = function(formula, train, test, thresh, seed, ntrees = 5000, shrink =
   test$State = ifelse(test$State == 1, "AFIB", "N")
   
   # Returns actual and predicted states.
-  return(list("Truth" = test$State, "Pred" = factor(pred, levels = c("N", "AFIB"))))
+  return(list("Truth" = factor(test$State, levels = c("N", "AFIB")), "Pred" = factor(pred, levels = c("N", "AFIB"))))
 }
 
-# Currently working on items below ###########################
+# LightGBM testing.
+boost_lgb = function(index_c, index_r, train, test, nrounds = 100, type = "gbdt", 
+                     num_leaves = 31, max_depth = -1, num_threads = 0, thresh, seed){
+  # Reclassifies states such that they are binary.
+  train$State = ifelse(train$State == "AFIB", 1, 0)
+  test$State = ifelse(test$State == "AFIB", 1, 0)
+  
+  # Converts training set to a lgb.Dataset object for training.
+  dtrain = lgb.Dataset(data = as.matrix(train[, index_c]), label = train[, index_r])
+  
+  # Trains model with training data.
+  set.seed(seed)
+  mod = lgb.train(data = dtrain, boosting = type, num_leaves = num_leaves, nrounds = nrounds, 
+                  objective = "binary", max_depth = max_depth, verbose = 0, num_threads = num_threads,
+                  force_row_wise = T)
+  pred = predict(mod, as.matrix(test[, index_c]))
+  pred = ifelse(pred >= thresh, "AFIB", "N")
+  
+  test$State = ifelse(test[, index_r] == 1, "AFIB", "N")
+  
+  # Returns actual and predicted states.
+  return(list("Truth" = factor(test$State, levels = c("N", "AFIB")), "Pred" = factor(pred, levels = c("N", "AFIB"))))
+}
 
 # XGBoost testing.
-boost_xgb = function(index_r, index_c, train, test, nrounds, eta, gamma, max_depth, min_child_weight, subsample, colsample_bytree, lambda, alpha){
+boost_xgb = function(index_c, index_r, train, test, nrounds, eta, gamma, max_depth, min_child_weight, subsample, colsample_bytree, lambda, alpha, thresh, seed){
   # Reclassifies states such that they are binary. Converts data frames to matrices.
   train$State = ifelse(train$State == "AFIB", 1, 0); train = data.matrix(train)
   test$State = ifelse(test$State == "AFIB", 1, 0); test = data.matrix(test)
   
   # Trains model with training data.
   set.seed(seed)
-  xgb = xgboost(data = as.matrix(train[, index_c]), label = train[, 5], 
+  mod = xgboost(data = as.matrix(train[, index_c]), label = train[, index_r], 
                 nrounds = nrounds, eta = eta, gamma = gamma,
                 max_depth = max_depth, min_child_weight = min_child_weight, 
                 subsample = subsample, colsample_bytree = colsample_bytree, 
                 lambda = lambda, alpha = alpha, 
-                scale_pos_weight = sum(train[, 5] == 0) / sum(train[, 5] == 1),
-                objective = "binary:logistic", verbose = 0)
-  xgb_pred = predict(xgb, as.matrix(test[, index]))
-  xgb_pred = ifelse(xgb_pred >= 0.5, "AFIB", "N")
+                scale_pos_weight = sum(train[, index_r] == 0) / sum(train[, index_r] == 1),
+                objective = "reg:squarederror", verbose = 0)
+  pred = predict(mod, as.matrix(test[, index_c]))
+  pred = ifelse(pred >= thresh, "AFIB", "N")
   
-  test[,5] = ifelse(test[,5] == 1, "AFIB", "N")
+  test[, index_r] = ifelse(test[, index_r] == 1, "AFIB", "N")
   
-  return(list(mean(xgb_pred == test[,5]), test[,5], xgb_pred, Sys.time() - start))
+  # Returns actual and predicted states.
+  return(list("Truth" = factor(test[, index_r], levels = c("N", "AFIB")), "Pred" = factor(pred, levels = c("N", "AFIB"))))
 }
 
+# Support-Vector Machine testing.
+svm_f = function(formula, index_c, train, test, kernel = "radial", cost = 1, seed){
+  # Trains model with training data.
+  set.seed(seed)
+  mod = svm(form, data = train, kernel = kernel, cost = cost, scale = FALSE)
+  
+  # Classifies test data based on training model.
+  pred = predict(mod, test[, index_c])
+
+  # Returns actual and predicted states.
+  return(list("Truth" = test$State, "Pred" = factor(pred, levels = c("N", "AFIB"))))
+}
+
+# Random forest testing.
+# Linear discriminant analysis testing.
+rf_f = function(formula, train, test, ntrees, seed){
+  # Trains model with training data.
+  set.seed(seed)
+  mod = randomForest(form, data = train, ntree = ntrees)
+  
+  # Classifies test data based on training model.
+  pred = predict(mod, test)
+  
+  # Returns actual and predicted states.
+  return(list("Truth" = test$State, "Pred" = pred))
+}
+
+################################################################################
--- Code/STAGE3training.R
+++ Code/STAGE3training.R
@@ -6,29 +6,42 @@
 
 #### SPECIFICATIONS ####################################################################################################
 
-VERSION = "M" # M for MIT-BIH, C for 2017 CinC dataset
-K = 5 # folds of cross-validation (only for 2017 preferably)
-SEED = 3 # seed for running machine learning algorithms
-DIR = "C:/Users/mario/Documents/Internship and REU Stuff/UNCW 2019/Atrial_Fibrillation/Summer 2021 Testing/"
+VERSION = "C"   # M for MIT-BIH, C for 2017 CinC dataset
+K = 5           # folds of cross-validation (only for 2017 preferably)
+SEED = 3        # seed for running machine learning algorithms
+DIR = "C:/Users/mario/Documents/Internship and REU Stuff/UNCW 2019/Atrial_Fibrillation/Summer 2021 Testing/" # directory
 
-# LOG = logistic regression, LDA = linear discriminant analysis
-# QDA = quadratic discriminant analysis, GBM = gradient boosting
-# XGB = XGBoost, LGB = light gradient boosting
-TESTS = c("LOG", "LDA", "QDA")#, "GBM") 
+# Algorithms to run
+# LOG = logistic regression
+# LDA = linear discriminant analysis
+# QDA = quadratic discriminant analysis
+# GBM = gradient boosting
+# XGB = XGBoost
+# LGB = light gradient boosting
+# SVM = support-vector machine
+# RFO = random forest
+# vector of codes below (for TESTS)
+TESTS = c("LOG", "LDA", "QDA", "GBM", "XGB", "LGB", "SVM", "RFO")
+#TESTS = c("SVM")
 
+# Indices of covariates to use in model
 # (Intervals, HeartRate, S.S, S.Reg, S.L, Reg.S, Reg.Reg, Reg.L, 
 #      6          7       8     9     10    11      12      13
-# L.S, L.Reg, L.L, RRVar, dRRVar, dRRMean, SampEnt, NECRate)
-#  14   15    16    17      18      19       20       21
-COVARIATES = 21
+# L.S, L.Reg, L.L, RRVar, dRRVar, dRRMean, SampEnt, CoSEn, NECRate)
+#  14   15    16    17      18      19       20       21     22
+COVARIATES = c(8:16, 17, 18, 22)
 
-#### DATA COLLECTION ###################################################################################################
+# Index of response vector (State)
+RESPONSE = 2
 
-# Sets directory; gathers file with functions to run program.
+#### DATA GATHERING & PREPARATION ######################################################################################
+
+# Sets directory; gathers file with functions to run program; opens libraries needs to run models.
 setwd(DIR)
 source("Code/STAGE3functions.R")
+sapply(TESTS, library_selector)
 
-# Retrieves all R-R interval information for all subjects.
+# Retrieves all segment information for specific dataset.
 if (VERSION == "M"){ # "Data/MIT-BIH/seg" for MIT-BIH, "Data/2017/seg" for 2017 CinC dataset
   subpath = "Data/MIT-BIH/seg/"
   samp_data = read.csv(paste(subpath, "all_seg_data_MIT-BIH.csv", sep = ""))
@@ -37,15 +50,26 @@ if (VERSION == "M"){ # "Data/MIT-BIH/seg" for MIT-BIH, "Data/2017/seg" for 2017 
   samp_data = read.csv(paste(subpath, "all_seg_data_2017.csv", sep = ""))
 }
 
-#### CROSS-VALIDATION AND TESTING ######################################################################################
-
+# Converts state column from strings to factors.
 samp_data$State = factor(samp_data$State, levels = c("N", "AFIB"))
-groups = grouper(samp_data, VERSION, SEED, K)
+
+# Creates formula for models.
 form = formula_maker(COVARIATES, colnames(samp_data))
 
-res = lapply(TESTS, testing, data = samp_data, rand = groups, formula = form, 
+#### CROSS-VALIDATION AND TESTING ######################################################################################
+
+# Randomly assigns number to each observation for k-fold cross validation.
+groups = grouper(samp_data, VERSION, SEED, K)
+
+# For each algorithm, we use the testing function to run k-fold cross validation on the specified model
+res = lapply(TESTS, testing, data = samp_data, rand = groups, formula = form, index_c = COVARIATES, index_r = RESPONSE, 
            thresh = 0.5, seed = SEED)
 
-metrics(res[[2]])
+# Add names to the list of results.
+names(res) = TESTS
+
+# Calculate metrics indicating the success of the models and algorithms.
+metrics = lapply(res, metrics_f)
+metrics
 
 ########################################################################################################################
